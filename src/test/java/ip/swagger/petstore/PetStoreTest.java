0dd2f2f7b09fbe8a3dfd86068ea0442220fe41d7
Luc Wiering <lucwiering@hotmail.com>
2021-03-08 20:51:09 +0100

    added extra test

--- src/test/java/ip/swagger/petstore/PetStoreTest.java
+++ src/test/java/ip/swagger/petstore/PetStoreTest.java
@@ -10,6 +10,13 @@ public class PetStoreTest {
     @DisplayName("Basic Test")
     void basicTest(){
         System.out.println("This is a very basic test");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Basic Test 2")
+    void basicTest2(){
+        System.out.println("This is a second very basic test");
         Assertions.assertTrue(false);
     }
 }
